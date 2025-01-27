38f6febd28c33a805d5d83f610a69a1095d2dbfe
kvnbanunu <kvnbanunu@gmail.com>
2025-01-27 13:43:09 -0800

    Initial commit

---
+++ cli/go.mod
@@ -0,0 +1,3 @@
+module github.com/kvnbanunu/melke-playground/cli
+
+go 1.23.5
---
+++ cli/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// 'go run .' to run the program
+func main() {
+	fmt.Println("This is our test bed")
+}
